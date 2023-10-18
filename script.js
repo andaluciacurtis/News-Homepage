c2623c6e424477eca2228390780883115199a01e
andaluciacurtis <andalucia.curtis@gmail.com>
2023-10-18 15:03:05 -0700

    added X icon when nav is open

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const navToggle= document.querySelector(".mobile-nav-icon");
 const navBar = document.querySelector("nav");
 const darkBGHelper = document.querySelector(".darken-bg-helper");
+const closeIcon = document.querySelector(".close-icon");
 
 navToggle.addEventListener("click", ()=> {
   navBar.classList.toggle("open-nav");
   darkBGHelper.classList.toggle("appear");
+  closeIcon.classList.toggle("appear");
 })
