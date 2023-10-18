13a56e913dff4bb0dad77c20b0096023bd974d56
andaluciacurtis <andalucia.curtis@gmail.com>
2023-10-18 14:55:30 -0700

    animated mobile nav to slide

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navToggle= document.querySelector(".mobile-nav-icon");
 const navBar = document.querySelector("nav");
+const darkBGHelper = document.querySelector(".darken-bg-helper");
 
 navToggle.addEventListener("click", ()=> {
   navBar.classList.toggle("open-nav");
+  darkBGHelper.classList.toggle("appear");
 })
